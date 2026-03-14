b2b0ea5cba2a51387364b59cfc1d3bc4691814bf
rrrrbit <jon.macagba.main@gmail.com>
2026-03-14 18:47:07 +0000

    better (size-aware repulsion still broken)

--- FMP/Assets/ProjectName/Scripts/GameMaths.cs
+++ FMP/Assets/ProjectName/Scripts/GameMaths.cs
@@ -8,7 +8,7 @@ using UnityEngine.InputSystem.Utilities;
 
 public class GameMaths : MonoBehaviour
 {
-    public Dictionary<Node, Dictionary<Node, float>> nn;
+	public Dictionary<Node, Dictionary<Node, float>> nn;
 	public float centeringStrength;
 	public float dragStrength;
 	public float max;
@@ -20,7 +20,7 @@ public class GameMaths : MonoBehaviour
 
 
 	public Node[] nodes;
-    public TextMeshProUGUI debugText;
+	public TextMeshProUGUI debugText;
 
 	public VisualNode visualNodePrefab;
 
@@ -30,12 +30,12 @@ public class GameMaths : MonoBehaviour
 
 	public float desiredDistance = 10f;
 	private void Start()
-    {
-        //initialise list of all nodes
+	{
+		//initialise list of all nodes
 		nodes = new Node[startingNumber];
 		for (int i = 0; i < nodes.Length; i++)
 		{
-			var thisNode = new Node();
+			Node thisNode = new Node();
 			nodes[i] = thisNode;
 			thisNode.visual = Instantiate(visualNodePrefab, Random.insideUnitCircle, Quaternion.identity);
 		}
@@ -43,21 +43,20 @@ public class GameMaths : MonoBehaviour
 		//initialise nn matrix with random weights
 		nn = new Dictionary<Node, Dictionary<Node, float>>();
 		foreach (Node i in nodes)
-        {
-            Dictionary<Node, float> newRow = new Dictionary<Node, float>();
+		{
+			Dictionary<Node, float> newRow = new Dictionary<Node, float>();
 			foreach (Node j in nodes)
-            {
-				var x = Random.value * 2 - 1;
-				newRow.Add(j, Mathf.Pow(x, 5));
+			{
+				float x = Random.value * 2 - 1;
+				newRow.Add(j, Mathf.Pow(x, 7)*10);
 			}
 
-            nn.Add(i, newRow);
-        }
-    }
+			nn.Add(i, newRow);
+		}
+	}
 
-    private void FixedUpdate()
-    {
-		
+	private void FixedUpdate()
+	{
 		foreach (Node i in nodes)
 		{
 			i.indegree = nn[i].Values.Sum(x => Mathf.Abs(x));
@@ -65,21 +64,26 @@ public class GameMaths : MonoBehaviour
 			i.visual.indegree = i.indegree;
 			i.visual.connections = nn[i].Values.ToList();
 
+			Vector3 force = Vector3.zero;
 			foreach (Node j in nodes)
 			{
-				AccumulateForces(i, j, desiredDistance/* * (1 + 4 * (i.indegree - minIndegree) / (maxIndegree - minIndegree))*/, Time.fixedDeltaTime);
+				force += PairwiseForce(i, j, desiredDistance + i.visual.transform.localScale.x - 0.25f);
 
+				//debug edge visualisation
 				float gap = 0.01f;
-
 				Vector3 d = (j.visual.transform.position - i.visual.transform.position).normalized;
 				Vector3 offs = new(d.y, -d.x, 0);
-
-				
-
-				Debug.DrawLine(i.visual.transform.position + offs * gap, j.visual.transform.position + offs * gap, gradient.Evaluate( (nn[i][j] - min) / (max - min) ));
+				Debug.DrawLine(i.visual.transform.position + offs * gap, j.visual.transform.position + offs * gap, gradient.Evaluate((nn[i][j] - min) / (max - min)));
 			}
-			if(i.visual.v.Equals(new(float.NaN, float.NaN, float.NaN))) i.visual.v = Vector3.zero;
+			force += NodewiseForce(i);
+
+			if(!float.IsFinite(force.sqrMagnitude)) force = Vector3.zero;
+
+			i.visual.v += force * Time.fixedDeltaTime;
+			if (!float.IsFinite(i.visual.v.sqrMagnitude)) i.visual.v = Vector3.zero;
+
 			i.visual.transform.position += i.visual.v * Time.fixedDeltaTime;
+			if (!float.IsFinite(i.visual.transform.position.sqrMagnitude)) i.visual.transform.position = Vector3.zero;
 		}
 	}
 
@@ -94,32 +98,26 @@ public class GameMaths : MonoBehaviour
 		minIndegree = nodes.Min(i => nn[i].Values.Sum(x => Mathf.Abs(x)));
 	}
 
-	void AccumulateForces(Node i, Node j, float desiredDistance, float dt)
+	Vector3 PairwiseForce(Node i, Node j, float desiredDistance)
 	{
-		if(i == j) return;
+		if (i == j) return Vector3.zero;
 
-		var symmetricWeight = Mathf.Abs(nn[i][j]) / maxAbs / 2;
+		float symmetricWeight = Mathf.Abs(nn[i][j]) / maxAbs / 2;
 
 		Vector3 d = j.visual.transform.position - i.visual.transform.position;
 
 		Vector3 repulsion = desiredDistance * desiredDistance / (d.magnitude + 0.01f) * -d.normalized;
 		Vector3 attraction = d.sqrMagnitude / desiredDistance * d.normalized * symmetricWeight;
+
+		return attraction + repulsion;
+	}
+
+	Vector3 NodewiseForce(Node i)
+	{
 		Vector3 p = -i.visual.transform.position;
-		Vector3 centeringForce = centeringStrength * p.sqrMagnitude * p.normalized; // centering force
-		Vector3 drag = dragStrength * -i.visual.v.normalized * i.visual.v.sqrMagnitude; // drag
-
-		Vector3 force = 
-			attraction + 
-			repulsion + 
-			centeringForce +
-			drag
-		;
-
-		Vector3 newV = i.visual.v + force * dt;
-		i.visual.v = Mathf.Min(newV.magnitude, 100) * newV.normalized;
-		//i.visual.v += force * dt;
-
-		
+		Vector3 centeringForce = centeringStrength * p.sqrMagnitude * p.normalized;
+		Vector3 drag = dragStrength * -i.visual.v.normalized * i.visual.v.sqrMagnitude;
+		return centeringForce + drag;
 	}
 }
 

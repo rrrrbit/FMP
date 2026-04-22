42ab477d7e63ab6561dedf7e496bbf33d576d604
Jmitchell25-HYP <jmitchell25@elam.co.uk>
2026-04-22 10:57:04 +0100

    YEP

--- FMP/Assets/J0S!@H/Node stats.cs
+++ FMP/Assets/J0S!@H/Node stats.cs
@@ -2,13 +2,55 @@ using UnityEngine;
 using System.Collections.Generic;
 public class Nodestats : MonoBehaviour
 {
-    void UploadStats()
+    [SerializeField] Node Amplifier;
+    [SerializeField] Node stats;
+    [SerializeField] Node Node;
+    [SerializeField] Node Consilidation;
+    public class Stats
     {
+        public int stat1;
+        public int stat2;
+        public int stat3;
+        public int stat4;
+        public int stat5;
+
+
+
+
 
 
     }
 
+    public enum Stat
+    {
+        Physics2D,
+        Clarification,
+        Consildation,
+        NodeAmplifier,
+        Influences,
+        maxIndegree,
+        minIndegree,
+        Outdegree,
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 
     
 
@@ -16,6 +58,25 @@ public class Nodestats : MonoBehaviour
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
 

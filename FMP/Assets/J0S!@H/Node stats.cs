9c4158413dea29575c5901f8ee12024e49cd3c88
mmitchell25-dos <mmitchell25@elam.co.uk>
2026-04-24 12:44:30 +0100

    YES

--- FMP/Assets/J0S!@H/Node stats.cs
+++ FMP/Assets/J0S!@H/Node stats.cs
@@ -7,7 +7,9 @@ using UnityEditor.ShaderKeywordFilter;
 using UnityEngine.InputSystem;
 using UnityEngine.SocialPlatforms.Impl;
 public class Nodestats : MonoBehaviour
-{    
+{
+    [Header("Node Analytics")]
+    // This is just a title for the Node data
     [SerializeField] Node Amplifier;
     [SerializeField] Node stats;
     [SerializeField] Node Node;
@@ -16,7 +18,13 @@ public class Nodestats : MonoBehaviour
     [SerializeField] Node experimentation;
     private float elapsedTime = 0f;
     private float score = 0;
+    // It can also be a public float but I prefer to keep this confidential
     private float scoreMultiplier = 10f;
+     // Your score will multiply if you continue to influence other nodes
+
+    private bool Light_Yagami_is_Kira;
+    // This is to detect if the code implemented will reiterate
+   
     public class Stats
     {
         public int stat1;
@@ -37,7 +45,8 @@ public class Nodestats : MonoBehaviour
     
     public bool isNodeAnomalyDetected;
     public bool Node_Capacity = true;
-
+    public bool Node_disc = false;
+    public bool Node_demonstration = true;
     void Update()
     {
 
@@ -45,6 +54,7 @@ public class Nodestats : MonoBehaviour
         Debug.Log("Score: " + score);
         score = elapsedTime * scoreMultiplier;
         score = Mathf.FloorToInt(elapsedTime * scoreMultiplier); 
+        // This is me inputting a score system
 
 
 
@@ -52,22 +62,34 @@ public class Nodestats : MonoBehaviour
     public enum Statistics
     {
         Physics2D,
+        // Physics2D is a specialized simulation engine within game development (specifically Unity) used to managed two-dimensional Physical interactions. There's more to explain, but it will overcomplicate things
         Clarification,
+        // To give the node an understanding on how it's supposed to operate 
         Consildation,
+        // More nodes will unite and conquer
         NodeAmplifier,
+        // Node strength will increase if they either acquire a power-up or if they influence other nodes
         Influences,
+        // Used to make sure if there are positve/negative influences which either attract or repel
         maxIndegree,
         minIndegree,
         Outdegree,
         Node_velocity,
         Node_physics,
-
+        // How fast/slow paced the node will move when linked to other nodes
+        Node_Analytics,
+        // This is only for data purposes
     }
 
-  
+
     public void cancellation()
     {
+        do
+        {
 
+
+        } while (enabled);
+        // I need more explained in this bit of code, but it explains how nodes will repel each other if they share similarities
 
 
     }
@@ -75,12 +97,21 @@ public class Nodestats : MonoBehaviour
     
     public void Node_Sound_Amplitude()
     {
-        
+        Vector3[] targets = { Vector3.zero, Vector2.one, Vector3.up };
+        // This is for node sound volume and for how it travels
+        foreach (Vector3 target in targets)
+        {
+            //Input specific iteration 
+            // Still me experimenting
+        }
     }
 
     public void LIGHT_YAGAMI()
     {
-
+        while (gameObject.activeSelf)
+        {
+            // Code will continue to reiterate
+        }
 
 
     }
@@ -101,7 +132,7 @@ public class Nodestats : MonoBehaviour
             print("Node is validated");
 
         }
-
+        // These bits of code will ingrain this into the node which could improve their energy levels
     }
 
     public void HEISENBERG()
@@ -112,11 +143,44 @@ public class Nodestats : MonoBehaviour
         {
             print("Size is inadequate");
         }
+        else
+        {
+            print("Size is overextending");
+
+        } // There is a limit depending on what size the node reaches
+
 
     }
 
+    private void Node_disc_congregation()
+    {
+        if (Node_disc == true)
+        {
+            print("Disc is working");
 
 
+        }
+        else if (Node_disc == false)
+        {
+            print("Disc is not working");
+        }
+        // This is just me experimenting
+    }
+
+
+    public void Node_visuals()
+    {
+        if (Node_demonstration == true)
+            print("Demonstration is accurate");
+        else if (Node_demonstration == false)
+            print("Demonstration is inaccurate");
+        else
+        {
+            print("Demonstration is moderate");
+        }
+       // This is how I will 
+    }
+    
 
 
 

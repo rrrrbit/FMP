5f30933335b1924603671e5bda3ab3f4064c1b20
mmitchell25-dos <mmitchell25@elam.co.uk>
2026-04-30 11:04:49 +0100

    Right

--- FMP/Assets/ProjectName/Scripts/VisualNode.cs
+++ FMP/Assets/ProjectName/Scripts/VisualNode.cs
@@ -4,8 +4,32 @@ using System.Linq;
 using NUnit.Framework.Constraints;
 public class VisualNode : MonoBehaviour
 {
-	public int id;
-	public bool onScreen = true;
+	public Vector3 a;
+	public Vector3 v;
+	public Node node;
+	public GameMaths gameMaths;
+	public float outdegree;
+	public List<float> connections;
 
-	//public static implicit operator Node(VisualNode v) => v.node;
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnDrawGizmosSelected()
+	{
+		Vector3 weightedCentroid = Vector3.zero;
+		foreach (Node j in gameMaths.nodes)
+		{
+			weightedCentroid += j.visual.transform.position * j.outdegree * gameMaths.nn[node][j] / gameMaths.nodes.Sum(x => x.outdegree * gameMaths.nn[node][j]);
+		}
+		Gizmos.DrawSphere(weightedCentroid, 0.1f);
+	}
 }

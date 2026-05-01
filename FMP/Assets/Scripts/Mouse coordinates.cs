5a3f6bfd89bc9a3408a5db214f35cc3868deed0c
Jmitchell25-HYP <jmitchell25@elam.co.uk>
2026-05-01 15:47:23 +0100

    t5uw4y54

---
+++ FMP/Assets/Scripts/Mouse coordinates.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Mousecoordinates : MonoBehaviour
+{
+    void Start()
+    {
+
+
+
+
+    }
+
+    void Update()
+    {
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ FMP/Assets/Scripts/SaveData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+public class SaveData
+{
+    private static SaveData _current;
+    public static SaveData Current;
+
+
+
+
+
+
+
+}

2c3c6ed6028b9643af1cc4f1d8c752d793301a77
mmitchell25-dos <244543515+Jmitchell25-HYP@users.noreply.github.com>
2026-05-08 14:47:07 +0100

    asdws

---
+++ FMP/Assets/Scripts/Loops.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+
+public class Loops : MonoBehaviour
+{
+    int nodeCorrelation;
+    int numEnemies = 5;
+
+
+
+    private void Start()
+    {
+        while (nodeCorrelation < 0)
+        {
+            Debug.Log("The Nodes agree");
+            nodeCorrelation--;
+
+        }
+
+        for (int i = 0; i < numEnemies; i++)
+        {
+            Debug.Log("Analysing enemy number: " + i);
+
+        }
+
+
+
+    }
+    private void Update()
+    {
+       
+    }
+
+    
+
+}

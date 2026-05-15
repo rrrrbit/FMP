73473d2e720b9bd1c164f2dd54a0ad83825c0c49
mmitchell25-dos <244543515+Jmitchell25-HYP@users.noreply.github.com>
2026-05-15 14:45:54 +0100

    IDK

---
+++ FMP/Assets/__Dev/Josiah/Scripts/Options.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Options : MonoBehaviour
+{
+    public void OnClickOptions()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+
+
+
+
+
+
+
+
+}
